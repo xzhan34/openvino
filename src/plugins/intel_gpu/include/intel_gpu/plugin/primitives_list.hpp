0872ca3278eff183df80410001f86e3c7170fd9b
Li, Liang A <liang.a.li@intel.com>
2026-03-05 04:09:22 -0800

    Add FusedConv op support in runtime and Intel GPU plugin

--- src/plugins/intel_gpu/include/intel_gpu/plugin/primitives_list.hpp
+++ src/plugins/intel_gpu/include/intel_gpu/plugin/primitives_list.hpp
@@ -327,3 +327,4 @@ REGISTER_FACTORY(internal, GatedDeltaNet);
 REGISTER_FACTORY(internal, PagedCausalConv1D);
 REGISTER_FACTORY(internal, GatherMatmulCompressed);
 REGISTER_FACTORY(internal, LinearAttention);
+REGISTER_FACTORY(internal, FusedConv);
--- src/plugins/intel_gpu/src/graph/registry/registry.hpp
+++ src/plugins/intel_gpu/src/graph/registry/registry.hpp
@@ -139,6 +139,8 @@ REGISTER_IMPLS(eltwise);
 REGISTER_IMPLS(fake_convert);
 REGISTER_IMPLS(fully_connected);
 REGISTER_IMPLS(gated_mlp);
+REGISTER_IMPLS(fused_mlp);
+REGISTER_IMPLS(fused_conv);
 REGISTER_IMPLS(gather);
 REGISTER_IMPLS(gather_nd);
 REGISTER_IMPLS(gated_delta_net);
